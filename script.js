d7111361af214ec7c944c0ca96e623fc262e8596
Piotr Moryto <piotrmoryto@gmail.com>
2022-04-14 19:09:21 +0200

    fix effects on buttons and inputs

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const headingSpan2 = document.querySelector('.heading-span-2');
 
 document.querySelector('.signup-btn').addEventListener('click', () => {
     container.classList.add('change');
-    headingSpan2.textContent = 'Up';
+    setTimeout(() => {
+        headingSpan2.textContent = 'Up'; 
+    }, 200);
+    
 });
 
 document.querySelector('.signin-btn').addEventListener('click', () => {
     container.classList.remove('change')
-    headingSpan2.textContent = 'In';
+    setTimeout(() => {
+        headingSpan2.textContent = 'In'; 
+    }, 200);
 });
 
