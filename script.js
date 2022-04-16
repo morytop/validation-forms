144bbbeb763f298d328de4e7731a75d3091772d6
Piotr Moryto <piotrmoryto@gmail.com>
2022-04-16 18:13:47 +0200

    add validation

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const container = document.querySelector('.container');
 const headingSpan2 = document.querySelector('.heading-span-2');
+const form = document.querySelector('.form');
+
+const clearForm = () => {
+    document.querySelectorAll('.form-input-wrapper').forEach((item) => {
+        item.className = 'form-input-wrapper';
+    })
+    form.reset();
+}
 
 document.querySelector('.signup-btn').addEventListener('click', () => {
     container.classList.add('change');
     setTimeout(() => {
         headingSpan2.textContent = 'Up'; 
     }, 200);
-    
+    form.className = 'form sign-up';
+    clearForm();    
 });
 
 document.querySelector('.signin-btn').addEventListener('click', () => {
@@ -14,5 +23,76 @@ document.querySelector('.signin-btn').addEventListener('click', () => {
     setTimeout(() => {
         headingSpan2.textContent = 'In'; 
     }, 200);
+    form.className = 'form sign-in';
+    clearForm();
 });
 
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+const error = (input, message) => {
+    const inputWrapper = input.parentElement;
+    inputWrapper.className = 'form-input-wrapper error';
+    inputWrapper.querySelector('.message').textContent = message;
+};
+
+const success = (input) => {
+    const inputWrapper = input.parentElement;
+    inputWrapper.className = 'form-input-wrapper success'
+};
+
+const checkEmail = (input) => {
+    const regEx =  /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+
+    if( regEx.test(input.value.trim())) {
+        success(input);
+    }else {
+        error(input, 'Email is not valid');
+    }
+}
+
+const checkRequiredFields = (inputArr) => {
+    inputArr.forEach(input => {
+        if (input.value.trim() === ""){
+            if(input.id === 'password2') {
+                error(input, 'Password confirmation is required')
+            } else {
+                error(input, `${input.id} is required`);
+            }
+        } else {
+            success(input);
+        }
+    });
+}
+
+const checkLenght = (input, min, max) => {
+    if (input.value.lenght < min) {
+        error(input, `${input.id} must be at least ${min} characters`); 
+    }else if (input.value.lenght > max) {
+        error(input, `${input.id} must be less than ${max} characters`)
+    }else {
+        success(input);
+    }
+}
+
+const passwordMatch = (input1, input2) => {
+    if(input1.value !== input2.value) {
+        error(input2, 'Passwords do not match');
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (form.classList[1] === 'sign-up') {
+    checkRequiredFields([username, email, password, password2]);
+    checkLenght(username, 2, 15);
+    checkLenght(password, 5, 25);
+    passwordMatch(password, password2);
+    }else {
+        checkRequiredFields([email, password]);
+    }
+    checkEmail(email);
+})
